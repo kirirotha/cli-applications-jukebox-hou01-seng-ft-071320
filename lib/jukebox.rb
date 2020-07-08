abe159e376b63fea7ef35b4c3d141cfef8c87a1a
Kiri Um <kirirotha@gmail.com>
2020-07-08 17:34:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,16 +8,38 @@ users_name = gets.strip
  
 puts say_hello(users_name)
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+
 def help
-  
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 def play
   
 end
 
-def list
-
+def list(songs)
+  counter = 0
+  songs.each do |song|
+    puts "#{counter}. #{song}"
+    counter ++
+  end  
 end
 
 def exit_jukebox
